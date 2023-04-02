af3de974e229799446667de724ac2a611bb7a416
outstandyy <zinchenko.vladislav.1995@gmail.com>
2023-04-02 13:06:25 +0530

    finish module 4

---
+++ Module 4: Fundamentals of Machine Learning/the final challenge/main.py
@@ -0,0 +1,51 @@
+import numpy as np
+import mnist as data
+
+# Applying Logistic Regression
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+# Basically doing prediction but named forward as its
+# performing Forward-Propagation
+def forward(X, w):
+    weighted_sum = np.matmul(X, w)
+    return sigmoid(weighted_sum)
+
+# Calling the predict() function
+def classify(X, w):
+    y_hat = forward(X, w)
+    labels = np.argmax(y_hat, axis=1)
+    return labels.reshape(-1, 1)
+
+# Computing Loss over using logistic regression
+def loss(X, Y, w):
+    y_hat = forward(X, w)
+    first_term = Y * np.log(y_hat)
+    second_term = (1 - Y) * np.log(1 - y_hat)
+    return -np.sum(first_term + second_term) / X.shape[0]
+
+# calculating gradient
+def gradient(X, Y, w):
+    return np.matmul(X.T, (forward(X, w) - Y)) / X.shape[0]
+
+# Printing results to the terminal screen
+def report(iteration, X_train, Y_train, X_test, Y_test, w):
+    matches = np.count_nonzero(classify(X_test, w) == Y_test)
+    n_test_examples = Y_test.shape[0]
+    matches = matches * 100.0 / n_test_examples
+    training_loss = loss(X_train, Y_train, w)
+    if (iteration%20 == 0) or iteration == 199:
+        print("%d - Loss: %.20f, %.2f%%" % (iteration, training_loss, matches))
+
+# calling the training function for desired no. of iterations
+def train(X_train, Y_train, X_test, Y_test, iterations, lr):
+    w = np.zeros((X_train.shape[1], Y_train.shape[1]))
+    for i in range(iterations):
+        report(i, X_train, Y_train, X_test, Y_test, w)
+        w -= gradient(X_train, Y_train, w) * lr
+    report(iterations, X_train, Y_train, X_test, Y_test, w)
+    return w
+
+w = train(data.X_train, data.Y_train,
+          data.X_test, data.Y_test,
+          iterations=200, lr=1e-5)
